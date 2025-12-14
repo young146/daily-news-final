f0451b578a35cbab3adf96e6cdf4c6a8c990b69c
young146 <younghan146@gmail.com>
2025-12-14 21:00:00 +0700

    chore: clean up build configs, revert lint ignores

--- next.config.mjs
+++ next.config.mjs
@@ -41,17 +41,6 @@ const nextConfig = {
       allowedOrigins: ['*'],
     },
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    ignoreBuildErrors: true,
-  },
 };
 
 export default nextConfig;
